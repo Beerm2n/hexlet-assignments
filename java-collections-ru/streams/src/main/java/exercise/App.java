decd15a1ef37be4815a3409dd8e576026e698463
@hexlet/cli <support@hexlet.io>
2023-04-18 00:34:53 +0300

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -1,15 +1,22 @@
 package exercise;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
 
-    public static Integer getCountOfFreeEmails(List<String> emails) {
-        List<String> freeHosts = List.of("@yandex.ru", "@gmail.com", "@hotmail.com");
-        long amountOfFreeEmails = emails.stream()
-                .filter(email -> freeHosts.stream().anyMatch(freehost -> email.contains(freehost)))
+    public static long getCountOfFreeEmails(List<String> emails) {
+        List<String> freeHosts = Arrays.asList("yandex.ru", "gmail.com", "hotmail.com");
+        return emails
+                .stream()
+                .map(email -> email.split("@")[1])
+                .filter(freeHosts::contains)
                 .count();
-        return (int) amountOfFreeEmails;
+    /*    Set<String> freeHosts = Set.of("yandex.ru", "gmail.com", "hotmail.com");
+        return (int) emails.stream()
+                .map(email -> email.substring(email.indexOf('@') + 1))
+                .filter(freeHosts::contains)
+                .count(); */
     }
 
 }
